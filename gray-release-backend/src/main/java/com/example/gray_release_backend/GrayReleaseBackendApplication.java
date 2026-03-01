177c91d4752c3fefe04e2c1605c6785f035195e4
周雄海 <herosea.zhou@gmail.com>
2026-03-01 21:35:56 +0800

    feat: 初始化

---
+++ gray-release-backend/src/main/java/com/example/gray_release_backend/GrayReleaseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gray_release_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrayReleaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrayReleaseBackendApplication.class, args);
+	}
+
+}
---
+++ gray-release-backend/src/test/java/com/example/gray_release_backend/GrayReleaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gray_release_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrayReleaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
